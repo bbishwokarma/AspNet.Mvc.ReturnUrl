182ff4da2ba9ec0e93efbfcc1a1d2f2f10e3f3e3
Bikash Bishwokarma <bikash.bishwokarma@gmail.com>
2017-07-21 12:06:09 -0500

    Improvements needed for creating nuget package.

--- AspNet.Mvc.RedirectAssist/Properties/AssemblyInfo.cs
+++ AspNet.Mvc.RedirectAssist/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AspNet.Mvc.ReturnUrl")]
+[assembly: AssemblyTitle("AspNet.Mvc.RedirectAssist")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AspNet.Mvc.ReturnUrl")]
+[assembly: AssemblyProduct("AspNet.Mvc.RedirectAssist")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
